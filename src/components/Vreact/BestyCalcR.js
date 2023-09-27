77902f3045227653f91e632b888dbcf23d324fb9
nathan-dopko <76851885+nathan-dopko@users.noreply.github.com>
2023-09-27 17:23:47 -0400

    added sams suggestions and a small bit of styles

--- src/components/Vreact/BestyCalcR.js
+++ src/components/Vreact/BestyCalcR.js
@@ -4,23 +4,30 @@ import "./styles.css";
 export const BestyCalcR = () => {
   const [isYearly, setIsYearly] = useState(false);
   const [listings, setListings] = useState(null);
-  const [totalCost, setTotalCost] = useState(0);
+  const [totalCost, setTotalCost] = useState(null);
   const [listingBreakdown, setListingBreakdown] = useState({});
-  const [avgCost, setAvgCost] = useState(0);
+  const [avgCost, setAvgCost] = useState(null);
 
   const pricingData = [
-    { from: 1, to: 10, cost: "$12 for the first 10 listings", price: 12 },
-    { from: 11, to: 20, cost: "$10 for each listing after 10th", price: 10 },
-    { from: 21, to: 50, cost: "$8 for each listing after 20th", price: 8 },
-    { from: 51, to: 100, cost: "$7 for each listing after 50th", price: 7 },
-    { from: 101, to: 200, cost: "$6 for each listing after 100th", price: 6 },
-    { from: 201, to: 400, cost: "$5 for each listing after 200th", price: 5 },
-    { from: 401, to: 500, cost: "$4 for each listing after 400th", price: 4 },
-    { from: 500, to: null, cost: "Contact Us", price: null },
+    { from: 1, to: 10, cost: "$12", price: 12 },
+    { from: 11, to: 20, cost: "$10", price: 10 },
+    { from: 21, to: 50, cost: "$8", price: 8 },
+    { from: 51, to: 100, cost: "$7", price: 7 },
+    { from: 101, to: 200, cost: "$6", price: 6 },
+    { from: 201, to: 400, cost: "$5", price: 5 },
+    { from: 401, to: 500, cost: "$4", price: 4 },
+    { from: 500, to: "", cost: "Contact Us", price: null },
   ];
 
   const calculateCostAndBreakdown = () => {
-    let cost = null;
+    if (listings === null) {
+      setTotalCost(null);
+      setListingBreakdown({});
+      setAvgCost(null);
+      return;
+    }
+
+    let cost = 0;
     let remainingListings = listings;
     let breakdown = {};
 
@@ -35,10 +42,11 @@ export const BestyCalcR = () => {
         remainingListings -= consumed;
       }
     }
-
     setTotalCost(remainingListings > 0 ? "Contact Us" : isYearly ? cost * 12 : cost);
     setListingBreakdown(breakdown);
-    setAvgCost(isYearly ? (cost / listings) * 12 : cost / listings);
+    if (listings > 0) {
+      setAvgCost(isYearly ? (cost / listings) * 12 : cost / listings);
+    }
   };
 
   useEffect(() => {
@@ -48,37 +56,46 @@ export const BestyCalcR = () => {
   return (
     <div className="calculatorWrapper">
       <div className="headerWrapper">
-        <div className="title">BestyCalcR</div>
-        <button onClick={() => setIsYearly(!isYearly)}>{isYearly ? "Switch to Monthly" : "Switch to Yearly"}</button>
-      </div>
-      <div className="inputWrapper">
-        <input type="number" value={listings} onChange={(e) => setListings(Number(e.target.value))} className="listingInput" />
-        <button onClick={calculateCostAndBreakdown} className="calcButton">
-          Calculate
+        <div className="title">Pricing Calculator</div>
+        <button onClick={() => setIsYearly(!isYearly)} className="yearlySwapBtn">
+          {isYearly ? "Switch to Monthly" : "Switch to Yearly"}
         </button>
       </div>
-      <div className="resultWrapper">Monthly Cost: {totalCost === "Contact Us" ? totalCost : `$${totalCost}`}</div>
-      <div>Avg Listing Cost: ${avgCost.toFixed(2)}</div>
+      <div className="inputWrapper">
+        <input
+          type="number"
+          value={listings === null ? "" : listings}
+          onChange={(e) => setListings(e.target.value === "" ? null : Number(e.target.value))}
+          className="listingInput"
+          placeholder="Enter listing count"
+        />
+      </div>
       <table className="pricingTable">
         <thead>
           <tr>
-            <th>From</th>
-            <th>To</th>
-            <th>Monthly Cost Per Listing</th>
-            <th>My Listing Count</th>
+            <th>Listings</th>
+            <th>Cost/Listing</th>
+            <th>My Listings</th>
+            <th>My Cost</th>
           </tr>
         </thead>
         <tbody>
           {pricingData.map((row, index) => (
             <tr key={index}>
-              <td>{row.from}</td>
-              <td>{row.to !== null ? row.to : "500+"}</td>
+              <td>{row.from === 500 ? "500+" : `${row.from} - ${row.to}`}</td>
               <td>{row.cost}</td>
-              <td>{listingBreakdown[row.from] || "-"}</td>
+              <td>{listings > 500 && row.from === 500 ? "Contact Us" : listingBreakdown[row.from] || "-"}</td>
+              <td>{listings > 500 && row.from === 500 ? "Contact Us" : listingBreakdown[row.from] ? `$${listingBreakdown[row.from] * row.price}` : "-"}</td>
             </tr>
           ))}
         </tbody>
       </table>
+      <div className="contentWrapper">
+        <div className="avgListings">
+          Monthly Cost: {totalCost ? (listings > 500 ? "Contact Us" : totalCost === "Contact Us" ? totalCost : `$${totalCost.toLocaleString()}`) : "$0"}
+        </div>
+        <div className="avgListings">Avg Listing Cost: {avgCost ? (listings > 500 ? "Contact Us" : `$${avgCost.toFixed(2)}`) : "0"}</div>
+      </div>
     </div>
   );
 };
