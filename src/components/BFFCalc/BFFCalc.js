d0867d74b839703efeeb9b4a049592fe40827aba
nathan-dopko <76851885+nathan-dopko@users.noreply.github.com>
2025-02-06 15:06:40 -0500

    update to pricing

--- src/components/BFFCalc/BFFCalc.js
+++ src/components/BFFCalc/BFFCalc.js
@@ -13,9 +13,9 @@ import { BestyCalc } from "../Vreact/BestyCalcR";
 // ];
 
 const pricingData = [
-  { from: 1, to: 10, cost: "$18", price: 18 },
-  { from: 11, to: 20, cost: "$15", price: 15 },
-  { from: 21, to: 50, cost: "$12", price: 12 },
+  { from: 1, to: 10, cost: "$20", price: 20 },
+  { from: 11, to: 50, cost: "$15", price: 15 },
+  // { from: 21, to: 50, cost: "$12", price: 12 },
   { from: 51, to: 100, cost: "$10", price: 10 },
   { from: 101, to: 250, cost: "$9", price: 9 },
   { from: 251, to: 499, cost: "$7", price: 7 },
